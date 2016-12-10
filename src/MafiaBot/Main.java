699ebb019be90a659bcc9a391c28db1f6c8cfc24
Ko Ro <k.romanyuk@gmail.com>
2016-12-11 02:41:22 +0300

    custom keyboard

--- src/MafiaBot/Main.java
+++ src/MafiaBot/Main.java
@@ -4,6 +4,7 @@ package MafiaBot;
  * Created by skydiver on 11.12.16.
  */
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,6 +16,7 @@ import org.telegram.telegrambots.api.objects.Message;
 import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.api.objects.replykeyboard.buttons.KeyboardButton;
 import org.telegram.telegrambots.api.objects.replykeyboard.buttons.KeyboardRow;
 
 public class Main extends TelegramLongPollingBot {
@@ -52,12 +54,59 @@ public class Main extends TelegramLongPollingBot {
         }
     }
 
+    private List<KeyboardRow> GetKeyboard(String KBType) {
+
+        List<KeyboardRow> keyboard = new ArrayList<>();
+
+        if (KBType == "main") {
+
+            KeyboardRow keyboardFirstRow = new KeyboardRow();
+            keyboardFirstRow.add("1");
+            keyboardFirstRow.add("2");
+            keyboardFirstRow.add("3");
+            keyboardFirstRow.add("4");
+
+            KeyboardRow keyboardSecondRow = new KeyboardRow();
+            keyboardSecondRow.add("5");
+            keyboardSecondRow.add("6");
+            keyboardSecondRow.add("7");
+            keyboardSecondRow.add("8");
+
+            KeyboardRow keyboardThirdRow = new KeyboardRow();
+            keyboardThirdRow.add("9");
+            keyboardThirdRow.add("10");
+            keyboardThirdRow.add("11");
+            keyboardThirdRow.add("12");
+
+            KeyboardRow keyboardfourthRow = new KeyboardRow();
+            keyboardfourthRow.add("Back");
+            keyboardfourthRow.add("Fouls");
+
+            keyboard.add(keyboardFirstRow);
+            keyboard.add(keyboardSecondRow);
+            keyboard.add(keyboardThirdRow);
+            keyboard.add(keyboardfourthRow);
+
+        }
+
+        return keyboard;
+
+    }
+
     private void sendMsg(Message message, String text) {
         SendMessage sendMessage = new SendMessage();
         sendMessage.enableMarkdown(true);
         sendMessage.setChatId(message.getChatId().toString());
         //sendMessage.setReplyToMessageId(message.getMessageId());
         sendMessage.setText(text);
+
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        //replyKeyboardMarkup.setOneTimeKeyboad(true);
+        replyKeyboardMarkup.setSelective(true);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+
+        replyKeyboardMarkup.setKeyboard(GetKeyboard("main"));
+        sendMessage.setReplyMarkup(replyKeyboardMarkup);
         try {
             sendMessage(sendMessage);
         } catch (TelegramApiException e) {
